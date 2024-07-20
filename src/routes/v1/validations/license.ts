ff06396305f03e53dc273f0a5de0c559d286fffa
Mathias Wagner <germannewsmaker@gmail.com>
2024-07-20 12:37:28 +0200

    Create license.ts validation

---
+++ src/routes/v1/validations/license.ts
@@ -0,0 +1,10 @@
+import Joi from "joi";
+
+export const licenseCreationValidation = Joi.object({
+    key: Joi.string().min(3).max(128),
+    groups: Joi.array().items(Joi.string().min(3).max(128)),
+    permissions: Joi.array().items(Joi.string().min(3).max(128)),
+    meta: Joi.object(),
+    maxUses: Joi.number().min(1).max(1000000),
+    expirationDate: Joi.date().min("now").max("1-1-2100").iso()
+});
