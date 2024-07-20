2ebcd8bccc5b15ae885f6bdcc5346cf318dd1311
Mathias Wagner <germannewsmaker@gmail.com>
2024-07-20 12:40:31 +0200

    Create project.ts validation

---
+++ src/routes/v1/validations/project.ts
@@ -0,0 +1,16 @@
+import Joi from "joi";
+
+export const projectCreationValidation = Joi.object({
+    name: Joi.string().alphanum().min(3).max(15).required()
+});
+
+export const patchProjectValidation = Joi.object({
+    name: Joi.string().alphanum().min(3).max(15),
+    defaults: Joi.object({
+        licenseKey: Joi.string(),
+        groups: Joi.array(),
+        permissions: Joi.array(),
+        expirationDate: Joi.date(),
+        maxUses: Joi.number().min(-1).max(100000),
+    })
+});
