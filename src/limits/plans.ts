968e7faff52ebb57c35f31140ac60d2f660a94f4
Mathias Wagner <germannewsmaker@gmail.com>
2024-07-20 12:30:37 +0200

    Create plan limits

---
+++ src/limits/plans.ts
@@ -0,0 +1,29 @@
+export const planLimits = {
+    account: {
+        FREE_PROJECTS: 5,
+    },
+    personal: {
+        LICENSES: 100,
+        MEMBERS: 1,
+        GROUPS: 3,
+        KEYS: 1,
+        META: 3,
+        PERMISSIONS: 10
+    },
+    plus: {
+        LICENSES: 5000,
+        MEMBERS: 10,
+        GROUPS: 15,
+        KEYS: 10,
+        META: 10,
+        PERMISSIONS: 50
+    },
+    pro: {
+        LICENSES: 50000,
+        MEMBERS: 20,
+        GROUPS: 40,
+        KEYS: 20,
+        META: 20,
+        PERMISSIONS: 100
+    }
+}
