ee3617f053ece496e328cf2f104651f47ef76c2c
Mathias Wagner <germannewsmaker@gmail.com>
2024-07-21 17:56:56 +0200

    Create javascript LicenseMeta.ts entity

---
+++ integrations/javascript/src/entities/LicenseMeta.ts
@@ -0,0 +1,21 @@
+export class LicenseMeta {
+    public readonly key: string;
+    public readonly value: string;
+
+    constructor(key: string, value: string) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public getAsInteger(): number {
+        return parseInt(this.value, 10);
+    }
+
+    public getAsDouble(): number {
+        return parseFloat(this.value);
+    }
+
+    public getAsBoolean(): boolean {
+        return this.value.toLowerCase() === 'true';
+    }
+}
