fd519cd93d46bdcb16289b19ca6368ecd07641a7
Mathias Wagner <germannewsmaker@gmail.com>
2024-07-22 13:05:07 +0200

    Show signupEnabled status in info.ts

--- src/routes/v1/info.ts
+++ src/routes/v1/info.ts
@@ -3,7 +3,8 @@ import { Request, Response, Router } from "express";
 const app: Router = Router();
 
 app.get("/status", (req: Request, res: Response) => {
-    res.json({ service: "LicenseAPI Backend", version: process.env.PROJECT_VERSION, status: "online" });
+    res.json({ service: "LicenseAPI Backend", version: process.env.PROJECT_VERSION, status: "online",
+        signupEnabled: process.env.DISABLE_SIGNUPS !== "true" });
 });
 
 export default app;
