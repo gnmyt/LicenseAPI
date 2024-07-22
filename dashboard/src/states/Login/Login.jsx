020a7a4e91553af8761529499032e1239f86baf4
Mathias Wagner <germannewsmaker@gmail.com>
2024-07-22 13:05:55 +0200

    Implement signupEnabled in Login.jsx

--- dashboard/src/states/Login/Login.jsx
+++ dashboard/src/states/Login/Login.jsx
@@ -1,11 +1,13 @@
 import {Stack, Tab, Tabs} from "@mui/material";
 import Logo from "@/states/Login/components/Logo";
-import {useState} from "react";
+import {useContext, useState} from "react";
 import LoginTab from "@/states/Login/tabs/LoginTab";
 import RegisterTab from "@/states/Login/tabs/RegisterTab";
+import {InfoContext} from "@contexts/Info";
 
 export const Login = () => {
     const [currentPage, setCurrentPage] = useState(0);
+    const {info} = useContext(InfoContext);
 
     const [showTabs, setShowTabs] = useState(true);
 
@@ -13,7 +15,7 @@ export const Login = () => {
         <Stack justifyContent="center" alignItems="center" sx={{height: {xs: "100%",sm: "100vh"}}} gap={2}>
             <Logo/>
 
-            {showTabs && <Tabs value={currentPage} onChange={(e, v) => setCurrentPage(v)}>
+            {info.signupEnabled && showTabs && <Tabs value={currentPage} onChange={(e, v) => setCurrentPage(v)}>
                 <Tab label="Login"/>
                 <Tab label="Register"/>
             </Tabs>}
