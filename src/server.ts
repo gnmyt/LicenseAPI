dc5a29639077fbf47e19952ddd926a2c27c57fc8
Mathias Wagner <germannewsmaker@gmail.com>
2024-07-22 20:19:03 +0200

    Implement projectVersion in server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,15 @@ import { sendError } from "@utils/error";
 import cors from "cors";
 import path from "node:path";
 import env from "./templates/env";
+import packageJson from "../package.json";
 
 const MONGOOSE_STRING = process.env.MONGOOSE_STRING || "mongodb://localhost:27017";
 
 const app: Application = express();
 const port: number = parseInt(process.env.SERVER_PORT || "8025");
 const isDevelopment: boolean = process.env.NODE_ENV !== "production";
+
+process.env.PROJECT_VERSION = isDevelopment ? "DEV" : packageJson.version;
 
 // Configure backend
 app.disable("x-powered-by");
@@ -24,7 +27,7 @@ app.use("/api/v1/", v1Router);
 
 app.use("/api/*", (_req: Request, res: Response) => sendError(res, 404, 0, "The provided route could not be found"));
 
-if (process.env.NODE_ENV === "production") {
+if (!isDevelopment) {
     app.use(express.static(path.join(__dirname, "../build")));
 
     app.get("*", (_req, res) => res.sendFile(path.join(__dirname, "../build", "index.html")));
