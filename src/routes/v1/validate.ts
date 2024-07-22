b547e3054b2b6059e5f2ef9b307ef09be20a1f53
Mathias Wagner <germannewsmaker@gmail.com>
2024-07-23 00:05:06 +0200

    Add /validate/:licenseKey/sign route

--- src/routes/v1/validate.ts
+++ src/routes/v1/validate.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import { validateLicense } from "@controller/validation";
+import { validateLicense, signOfflineKey } from "@controller/validation";
 
 const app: Router = Router();
 
@@ -8,6 +8,20 @@ app.get("/:licenseKey", async (req: Request, res: Response) => {
     if (!validationKey) return res.status(400).json({ code: 1, message: "You need to provide a validation key" });
 
     res.json(await validateLicense(validationKey, req.params.licenseKey));
+});
+
+app.get("/:licenseKey/sign", async (req: Request, res: Response) => {
+    const validationKey = req.header("X-Validation-Key");
+    if (!validationKey) return res.status(400).json({ code: 1, message: "You need to provide a validation key" });
+
+    const offlineKey = await signOfflineKey(validationKey, req.params.licenseKey);
+
+    if (offlineKey.status !== "VALID") return res.json(offlineKey);
+
+    res.header("Content-Type", "application/octet-stream")
+        .header("Content-Disposition", `attachment; filename=${req.params.licenseKey}.lkey`)
+        .send(offlineKey.file);
+
 });
 
 export default app;
