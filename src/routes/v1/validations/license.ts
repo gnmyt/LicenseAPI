946c9ef2035dbb9d10d015d8ad626b3ab857c8fa
Mathias Wagner <germannewsmaker@gmail.com>
2024-07-22 17:19:18 +0200

    Fix license.ts validation

--- src/routes/v1/validations/license.ts
+++ src/routes/v1/validations/license.ts
@@ -6,5 +6,5 @@ export const licenseCreationValidation = Joi.object({
     permissions: Joi.array().items(Joi.string().min(3).max(128)),
     meta: Joi.object(),
     maxUses: Joi.number().min(1).max(1000000),
-    expirationDate: Joi.date().min("now").max("1-1-2100").iso()
+    expirationDate: Joi.date().min("now").max("1-1-2100").iso().allow(0)
 });
