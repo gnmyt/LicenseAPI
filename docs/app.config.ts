73c7bc5e7708077ed8471cb6def3d4512fd07d6a
Mathias Wagner <germannewsmaker@gmail.com>
2024-07-30 15:21:37 +0200

    Implement docs

---
+++ docs/app.config.ts
@@ -0,0 +1,32 @@
+// https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
+export default defineAppConfig({
+  docus: {
+    title: 'LicenseAPI',
+    description: 'The best place to start your documentation.',
+    socials: {
+      github: 'gnmyt/LicenseAPI'
+    },
+    github: {
+      dir: 'docs/content',
+      branch: 'main',
+      repo: 'LicenseAPI',
+      owner: 'gnmyt',
+      edit: true
+    },
+    aside: {
+      level: 0,
+      collapsed: false,
+      exclude: []
+    },
+    main: {
+      padded: true,
+      fluid: true
+    },
+    header: {
+      logo: true,
+      showLinkIcon: true,
+      exclude: [],
+      fluid: true
+    }
+  }
+})
