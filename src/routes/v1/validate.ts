fece14b142695565ddcf483db70db2ebcf62d424
Mathias Wagner <germannewsmaker@gmail.com>
2024-07-30 21:43:16 +0200

    Update signing in validate.ts routes

--- src/routes/v1/validate.ts
+++ src/routes/v1/validate.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import { validateLicense, signOfflineKey } from "@controller/validation";
+import {validateLicense, signOfflineKey, signKey} from "@controller/validation";
 
 const app: Router = Router();
 
@@ -14,6 +14,13 @@ app.get("/:licenseKey/sign", async (req: Request, res: Response) => {
     const validationKey = req.header("X-Validation-Key");
     if (!validationKey) return res.status(400).json({ code: 1, message: "You need to provide a validation key" });
 
+    res.json(await signKey(validationKey, req.params.licenseKey));
+});
+
+app.get("/:licenseKey/offline", async (req: Request, res: Response) => {
+    const validationKey = req.header("X-Validation-Key");
+    if (!validationKey) return res.status(400).json({ code: 1, message: "You need to provide a validation key" });
+
     const offlineKey = await signOfflineKey(validationKey, req.params.licenseKey);
 
     if (offlineKey.status !== "VALID") return res.json(offlineKey);
@@ -21,7 +28,6 @@ app.get("/:licenseKey/sign", async (req: Request, res: Response) => {
     res.header("Content-Type", "application/octet-stream")
         .header("Content-Disposition", `attachment; filename=${req.params.licenseKey}.lkey`)
         .send(offlineKey.file);
-
 });
 
 export default app;
