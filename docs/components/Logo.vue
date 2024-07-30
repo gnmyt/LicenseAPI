4b2fd71fed37ff9448a6028b4ea6ab5654e7a23c
Mathias Wagner <germannewsmaker@gmail.com>
2024-07-30 15:45:14 +0200

    Update Logo.vue

--- docs/components/Logo.vue
+++ docs/components/Logo.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+</script>
+
 <template>
-  <img src="/licenseapi.png"/>
+
+  <img src="/licenseapi-dark.png" v-if="colorMode.value === 'light'" alt="LicenseAPI Logo" />
+  <img src="/licenseapi.png" v-else alt="LicenseAPI Logo" />
 </template>
