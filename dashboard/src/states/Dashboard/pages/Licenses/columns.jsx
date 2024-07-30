fb678d190dc17bcbfc28d7b9f155cc61f582aade
Mathias Wagner <germannewsmaker@gmail.com>
2024-07-30 21:43:27 +0200

    Remove maxUses from columns.jsx

--- dashboard/src/states/Dashboard/pages/Licenses/columns.jsx
+++ dashboard/src/states/Dashboard/pages/Licenses/columns.jsx
@@ -3,7 +3,7 @@ import {Delete, Edit, Key} from "@mui/icons-material";
 
 export default (deleteLicense, editLicense) => ([
     {
-        field: 'key', headerName: 'License key', width: 230, renderCell: (params) => <Stack direction="row" gap={1}
+        field: 'key', headerName: 'License key', width: 280, renderCell: (params) => <Stack direction="row" gap={1}
                                                                                             alignItems="center">
             <Key/>{params.value}
         </Stack>
@@ -13,7 +13,7 @@ export default (deleteLicense, editLicense) => ([
     {field: 'permissions', headerName: 'Permissions', width: 200, renderCell: (params) =>
             params.value.length > 0 ? params.value.join(', ') : "-"},
     {
-        field: "meta", headerName: "Meta-Data", width: 270, renderCell: (params) => <Stack direction="row" gap={1}
+        field: "meta", headerName: "Meta-Data", width: 400, renderCell: (params) => <Stack direction="row" gap={1}
                                                                                            height="100%"
                                                                                            alignItems="center">
             {Object.keys(params.value).map((key) => <Chip key={key} label={`${key}: ${params.value[key]}`}/>)}
@@ -21,8 +21,6 @@ export default (deleteLicense, editLicense) => ([
         </Stack>
     },
     {field: 'currentUses', headerName: 'Current uses', width: 180},
-    {field: 'maxUses', headerName: 'Maximum uses', width: 180, renderCell: (params) =>
-            params.value === -1 ? "Unlimited" : params.value},
     {
         field: 'expirationDate', headerName: 'Expiration date', width: 200, renderCell: (params) =>
             new Date(params.value).getTime() === 0 ? "Never" : new Date(params.value).toLocaleString()
